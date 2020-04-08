f98db82df0f40616aa4392529d26a907b0e29d6f
Leo-Marinho <leomarinho.is@icloud.com>
2020-04-08 10:30:27 -0300

    first commit

---
+++ src/main/java/com/biography/courses/BiographyCoursesApplication.java
@@ -0,0 +1,13 @@
+package com.biography.courses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiographyCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiographyCoursesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biography/courses/BiographyCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biography.courses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiographyCoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
